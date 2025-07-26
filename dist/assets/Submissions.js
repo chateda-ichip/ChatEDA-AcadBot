99da52478d0ae85bee30e872f2329546f39de475
Muz-IC <yuan-wan21@mails.tsinghua.edu.cn>
2025-07-27 00:54:49 +0800

    Add dist for GitHub Pages

---
+++ dist/assets/Submissions.js
@@ -0,0 +1,2 @@
+import{d as s,a,e as o}from"./main.js";const t=s({name:"Submissions",setup:()=>()=>a("div",{class:"container mx-auto px-4 py-8"},[a("h1",{class:"text-2xl font-bold mb-4"},[o("My Contributions")]),a("div",{class:"bg-white rounded-lg shadow p-6"},[a("p",{class:"text-gray-600"},[o("No submission record")])])])});export{t as default};
+//# sourceMappingURL=Submissions.js.map
