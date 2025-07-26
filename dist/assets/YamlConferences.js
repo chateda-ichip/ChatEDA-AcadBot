ece09b705b4f145acc82deee8ca41cc7b09b63cc
Muz-IC <yuan-wan21@mails.tsinghua.edu.cn>
2025-07-27 01:01:19 +0800

    xx

--- dist/assets/YamlConferences.js
+++
@@ -1,2 +0,0 @@
-import{d as a,a as s,e}from"./main.js";const t=a({name:"YamlConferences",setup:()=>()=>s("div",{class:"container mx-auto px-4 py-8"},[s("h1",{class:"text-2xl font-bold mb-4"},[e("YAML Meeting List")]),s("div",{class:"bg-white rounded-lg shadow p-6"},[s("p",{class:"text-gray-600"},[e("No meeting information")])])])});export{t as default};
-//# sourceMappingURL=YamlConferences.js.map
